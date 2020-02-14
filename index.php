3a4de6f64fcfe07335b7aef2e62b6f9ba5345c4a
Dushyant Joshi <dushyant.joshi@alogic.co>
2020-02-14 19:45:04 +1030

    Database Connection test

--- index.php
+++ index.php
@@ -1,10 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<body>
 <?php
-require 'vendor/autoload.php';
 
-use Carbon\Carbon;
+$hostname = "localhost";
+$username = "root";
+$password = "Kodinar@123";
+$db = "magento";
 
-$date = Carbon::now();
+$dbconnect = mysqli_connect($hostname, $username, $password, $db);
 
-echo $date->toDayDateTimeString();
+if ($dbconnect->connect_error) {
+	die("Database connection failed: " . $dbconnect->connect_error);
+}
 
-echo "Working with code deploy";
+?>
+
+<table border="1" align="center">
+<tr>
+  <td>ID</td>
+  <td>Name</td>
+  <td>Email</td>
+</tr>
+
+<?php
+
+$query = mysqli_query($dbconnect, "SELECT * FROM user")
+or die(mysqli_error($dbconnect));
+
+while ($row = mysqli_fetch_array($query)) {
+	echo
+		"<tr>
+    <td>{$row['id']}</td>
+    <td>{$row['name']}</td>
+    <td>{$row['email']}</td>
+   </tr>\n";
+
+}
+
+?>
+</table>
+</body>
+</html>
